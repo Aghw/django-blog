8b6f536efb3b9718a5796be8b0ab59c9d2393f9f
Aghw <sam.asmerom@outlook.com>
2020-11-07 03:30:49 -0800

    added more functionality to blogging to show lists and detail of each blog.

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+# # Define an inline class here
+# class CategoryAdmin(admin.ModelAdmin):
+#     pass
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Post, PostAdmin)
+# admin.site.register(Category, CategoryAdmin)
+
 admin.site.register(Post)
 admin.site.register(Category)
