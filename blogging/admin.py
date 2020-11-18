b55572fd621b4e402d11045a6a0ab4ef0a333e8b
Aghw <sam.asmerom@outlook.com>
2020-11-18 01:14:26 -0800

    Applied black to reformat code.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 class CategoryInline(admin.TabularInline):
     model = Category.posts.through
+
 
 # Define an inline class here
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     actions_on_bottom = True
-    empty_value_display = '-empty-'
-    fields = ('name', 'description')
+    empty_value_display = "-empty-"
+    fields = ("name", "description")
 
     inlines = [
         CategoryInline,
     ]
-    exclude = ('posts',)
+    exclude = ("posts",)
 
     def view_name(self, obj):
         return obj.name
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
