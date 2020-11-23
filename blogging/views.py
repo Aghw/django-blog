d82d688e891eb72290b46f2b30d5390a9850504b
Aghw <sam.asmerom@outlook.com>
2020-11-22 22:37:01 -0800

    removed extrac blank spaces.

--- blogging/views.py
+++ blogging/views.py
@@ -14,37 +14,14 @@ from blogging.forms import BlogPostForm
 
 class BlogListView(ListView):
     template_name = "blogging/list.html"
-    queryset = Post.objects.order_by("-published_date").exclude(
-        published_date__exact=None
+    queryset = Post.objects.exclude(published_date__exact=None).order_by(
+        "-published_date"
     )
 
 
 class BlogDetailView(DetailView):
     template_name = "blogging/detail.html"
     queryset = Post.objects.exclude(published_date__exact=None)
-
-
-# class BlogAddView(DetailView):
-#     template_name = "blogging/add.html"
-#     queryset = Post.objects.exclude(published_date__exact=None)
-
-#     def post(self, request, *args, **kwargs):
-#         blog = self.get_object()
-
-#         if request.method == "POST":
-#             form = BlogPostForm(request.POST)
-
-#             if form.is_valid():
-#                 model_instance = form.save(commit=False)
-#                 model_instance.published_date = timezone.now()
-#                 model_instance.save()
-#                 redirect('/')
-#             else:
-#                 return render(request, "blogging/add.html", {'form': form})
-#         else:
-#             form = BlogPostForm()
-
-#             return render(request, "blogging/add.html", {'form': form})
 
 
 @login_required
@@ -54,7 +31,6 @@ def add_model(request):
         form = BlogPostForm(request.POST or {})
 
         if form.is_valid():
-
             model_instance = form.save(commit=False)
             model_instance.author = request.user
             model_instance.title = request.POST.get("title")
